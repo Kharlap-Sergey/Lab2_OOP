c448170bc531a9f1eed9edf520cb049901dac1c3
snovis <sergeygoodgood@gmail.com>
2020-10-06 19:08:12 +0300

    SRP to the project

--- Lab2.v2/Program.cs
+++ Lab2.v2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab2.v2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,108 +8,11 @@ namespace Lab2
     class Program
     {
         static string ProgramPhelosofy { set; get; } = "C# is better then Java!!!";
-        private static object IsOperationImplemented(Func<object> action)
+
+        static void ShowTypeOperations(ICollection<string> operationsInfo, Type t)
         {
-            object result;
-            try
-            {
-                result = action?.Invoke();
-            }
-            catch (Exception e)
-            {
-                result = e.Message;
-            }
-            return result;
-        }
-        private static ICollection<string> CheckOperations(Type T)
-        {
-            dynamic a = Activator.CreateInstance(T);
-            dynamic b = Activator.CreateInstance(T);
-
-            Func<object>[] funcs =
-                {
-                    new Func<object>(() => a + b),
-                    new Func<object>(() => a += b),
-                    new Func<object>(() => a - b),
-                    new Func<object>(() => a -= b),
-                    new Func<object>(() => a * b),
-                    new Func<object>(() => a *= b),
-                    new Func<object>(() => a / b),
-                    new Func<object>(() => a /= b),
-                    new Func<object>(() => a ^ b),
-                    new Func<object>(() => a | b),
-                    new Func<object>(() => a & b),
-                    new Func<object>(() => a << 4),
-                    new Func<object>(() => a >> 4),
-                    new Func<object>(() => a && b),
-                    new Func<object>(() => a || b),
-                    new Func<object>(() => a > b),
-                    new Func<object>(() => a < b),
-                    new Func<object>(() => a == b),
-                    new Func<object>(() => a != b),
-                    new Func<object>(() => a <= b),
-                    new Func<object>(() => a >= b),
-                    new Func<object>(() => a++),
-                    new Func<object>(() => ++a),
-                    new Func<object>(() => --a),
-                    new Func<object>(() => a--),
-                     };
-
-
-            string[] operations =
-            {
-                    "a + b",
-                    "a += b",
-                    "a - b",
-                    "a -= b",
-                    "a * b",
-                    "a *= b",
-                    "a / b",
-                    " a /= b",
-                    " a ^ b",
-                    " a | b",
-                    " a & b",
-                    " a << 4",
-                    " a >> 4",
-                    " a && b",
-                    " a || b",
-                    " a > b",
-                    " a < b",
-                    " a == b",
-                    " a != b",
-                    " a <= b",
-                    " a >= b",
-                    " a++",
-                    " ++a",
-                    " --a",
-                    " a--"
-                     };
-
-            var joint = operations.Zip(funcs,
-                                      (op, func) => new
-                                      {
-                                          Op = op,
-                                          Func = func
-                                      }
-                                      );
-
-
-            var results = new List<string>();
-            foreach (var el in joint)
-            {
-                var res = IsOperationImplemented(el.Func);
-                var resStr = res ?? "null";
-                results.Add($"operation = {el.Op}, result = {resStr}");
-            }
-
-            return results;
-        }
-
-        static void ShowTypeOperations<T>()
-        {
-            Console.WriteLine(typeof(T));
-            var operationsResults = CheckOperations(typeof(T));
-            foreach (var operationRes in operationsResults)
+            Console.WriteLine(t);
+            foreach (var operationRes in operationsInfo)
             {
                 Console.WriteLine(operationRes);
             }
@@ -116,7 +20,9 @@ namespace Lab2
 
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(ProgramPhelosofy);
+            Console.ForegroundColor = ConsoleColor.Black;
             List<Type> types = new List<Type>()
             { 
                 typeof(int     ),
@@ -130,7 +36,11 @@ namespace Lab2
             };  
 
 
-            
+            foreach(var type in types)
+            {
+                var TypeOperationsDescriptions = TypeOperationsReflections.GetTypeOperations(type);
+                ShowTypeOperations(TypeOperationsDescriptions, type);
+            }
         }
     }
 }
---
+++ Lab2.v2/TypeOperationsReflections.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2.v2
+{
+    public static class TypeOperationsReflections
+    {
+        private static object GetOperationResult(Func<object> action)
+        {
+            object result;
+            try
+            {
+                result = action?.Invoke();
+            }
+            catch (RuntimeBinderException em)
+            {
+                return new String("Operation isn't implemented");
+            }
+            catch (Exception e)
+            {
+                result = "NaN";
+            }
+            return result;
+        }
+        public static ICollection<string> GetTypeOperations(Type T)
+        {
+            dynamic a = Activator.CreateInstance(T);
+            dynamic b = Activator.CreateInstance(T);
+
+            Func<object>[] funcs =
+                {
+                    new Func<object>(() => a = b),
+                    new Func<object>(() => a += b),
+                    new Func<object>(() => a - b),
+                    new Func<object>(() => a -= b),
+                    new Func<object>(() => a * b),
+                    new Func<object>(() => a *= b),
+                    new Func<object>(() => a / b),
+                    new Func<object>(() => a + b),
+                    new Func<object>(() => a /= b),
+                    new Func<object>(() => a ^ b),
+                    new Func<object>(() => a | b),
+                    new Func<object>(() => a & b),
+                    new Func<object>(() => a << 4),
+                    new Func<object>(() => a >> 4),
+                    new Func<object>(() => ~a),
+                    new Func<object>(() => !a),
+                    new Func<object>(() => a && b),
+                    new Func<object>(() => a || b),
+                    new Func<object>(() => a > b),
+                    new Func<object>(() => a < b),
+                    new Func<object>(() => a == b),
+                    new Func<object>(() => a != b),
+                    new Func<object>(() => a <= b),
+                    new Func<object>(() => a >= b),
+                    new Func<object>(() => !b),
+                    new Func<object>(() => a++),
+                    new Func<object>(() => ++a),
+                    new Func<object>(() => --a),
+                    new Func<object>(() => a--),
+                     };
+
+
+            string[] operations =
+            {
+                    "a = b",
+                    "a + b",
+                    "a += b",
+                    "a - b",
+                    "a -= b",
+                    "a * b",
+                    "a *= b",
+                    "a / b",
+                    " a /= b",
+                    " a ^ b",
+                    " a | b",
+                    " a & b",
+                    " a << 4",
+                    " a >> 4",
+                    " ~a",
+                    " !a",
+                    " a && b",
+                    " a || b",
+                    " a > b",
+                    " a < b",
+                    " a == b",
+                    " a != b",
+                    " a <= b",
+                    " a >= b",
+                    "!a",
+                    " a++",
+                    " ++a",
+                    " --a",
+                    " a--"
+                     };
+
+            var joint = operations.Zip(funcs,
+                                      (op, func) => new
+                                      {
+                                          Op = op,
+                                          Func = func
+                                      }
+                                      );
+
+
+            var results = new List<string>();
+            foreach (var el in joint)
+            {
+                var res = GetOperationResult(el.Func);
+                var resStr = res ?? "null";
+                results.Add($"operation = {el.Op}, result = {resStr}");
+            }
+
+            return results;
+        }
+    }
+}
